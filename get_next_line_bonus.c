69bcd3975a37328d0699b5071924fe50765a1630
Juan Carlos Alcausa Luque <160739943+jcalcausal@users.noreply.github.com>
2024-10-03 20:27:35 +0200

    Correción inclusión de cabecera

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*read_from_fd(int fd, char *buffer, char *buffer_remain)
 {
